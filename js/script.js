7fbb3dfa4e409292ee37eafd59647d3a43dcaf79
Itsnotpatricks <thiriez.patrick@gmail.com>
2026-05-14 20:58:58 +0200

    entrada /Bio

--- js/script.js
+++ js/script.js
@@ -5,3 +5,16 @@ function openMenu() {
 function closeMenu() {
     document.getElementById("menu").style.left = "-100%";
 }
+
+const input = document.getElementById("messageInput");
+const button = document.getElementById("sendBtn");
+
+input.addEventListener("input", () => {
+
+    if (input.value.trim() !== "") {
+        button.textContent = "Send it 🚀";
+    } else {
+        button.textContent = "Send Message";
+    }
+
+});
